0cbd4a0407cde452f82c014f1ecd520347364bd5
G0nz0pera <155404796+g0nz0pera@users.noreply.github.com>
2024-01-15 14:03:51 -0300

    final commit of the project in order to deploy it in an infra service

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import profilePic from "./rustIcon.png";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+import Icon from './Icon';
 
 function App() {
+  const supabase = createClient(
+      process.env.REACT_APP_SUPABASE_PROJ_URL,
+      process.env.REACT_APP_SUPABASE_PROJ_KEY
+      );
+
+  const [links, setLinks] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getLinks();
+  }, []);
+
+  async function getLinks() {
+    setLoading(true);
+    try {
+      const { data } = await supabase.from("links").select();
+      setLinks(data);
+    }catch(e){
+      console.error(e);
+    }finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header container mx-auto">
+        <img src={profilePic} className="profile-pic w-20 h-20 mb-4 rounded-full" alt="logo" />
+        {
+          loading ? <p>Please wait while I get my Links ready...</p> :
+              <ul className='flex flex-col w-full'>
+                {
+                    links && links.map((link, index) => (
+                        <li key={index}
+                            className='cursor-pointer m-2 py-2 px-10 border-black border-2 text-sm md:text-md text-slate-900 '>
+                          <a href={link.url} className='flex item-center' title={link.title} target='_blank'
+                             rel='no-referrer'>
+                            <Icon iconName={link.icon}/> <p className='ml-3'>{link.title}</p>
+                          </a>
+                        </li>
+                    ))
+                }
+              </ul>
+        }
+      </div>
     </div>
   );
 }
